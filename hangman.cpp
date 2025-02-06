f121c9522d4fd9e64e7120f95ec3d899820548d1
Nardos Anteneh Tilahun <audree489@gmail.com>
2025-02-06 09:12:16 +0300

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -23,3 +23,96 @@ void displayGameDetailes (int maxTries, int remainingTries){
 }
 //first part of the code, initializing and the printing stuff
 //yes it was indeed a pain in the ass to make 
+
+      /*  if (attemptsLeft == 9) {
+            cout<< " " <<endl;
+            cout<< " " <<endl;
+            cout<<"  " <<endl;
+            cout<< " " <<endl;
+            cout<< "  "<<endl;
+            cout<< "  "<<endl;
+            cout<< "  "<<endl;
+        }
+        else if (attemptsLeft == 8) {
+            cout<< "    " <<endl;
+            cout<< "  | " <<endl;
+            cout<< "  | " <<endl;
+            cout<< "  | "<<endl;
+            cout<< "  | " <<endl;
+            cout<< "  | " <<endl;
+            cout<< "  | " <<endl;
+        }
+        else if (attemptsLeft == 7) {
+            cout<< "   _____" <<endl;
+            cout<< "  |     " <<endl;
+            cout<< "  |     " <<endl;
+            cout<< "  |     " <<endl;
+            cout<< "  |     " <<endl;
+            cout<< "  |     " <<endl;
+            cout<< "  |     " <<endl;
+        }
+        else if (attemptsLeft == 6) {
+            cout<< "   _____" <<endl;
+            cout<< "  |     |" <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+        }
+        else if (attemptsLeft == 5) {
+            cout<< "   _____" <<endl;
+            cout<< "  |     |" <<endl;
+            cout<< "  |     O" <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |      " <<endl;
+        }
+        else if (attemptsLeft == 4) {
+            cout<< "   _____" <<endl;
+            cout<< "  |     |" <<endl;
+            cout<< "  |     O" <<endl;
+            cout<< "  |     |" <<endl;
+            cout<< "  |      " <<endl;
+            cout<< "  |" << endl;
+            cout<< "  |" << endl;
+        }
+        else if (attemptsLeft ==3){
+            cout<< "   _____"<<endl;
+            cout<< "  |     |"<<endl;
+            cout<< "  |     O"<<endl;
+            cout<< "  |    /|"<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl;   
+            }
+        else if (attemptsLeft ==2){
+            cout<< "   _____"<<endl;
+            cout<< "  |     |"<<endl;
+            cout<< "  |     O"<<endl;
+            cout<< "  |    /|\\"<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl; 
+        }
+        else if (attemptsLeft ==1){
+            cout<< "   _____"<<endl;
+            cout<< "  |     |"<<endl;
+            cout<< "  |     O"<<endl;
+            cout<< "  |    /|\\"<<endl;
+            cout<< "  |    /"<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl; 
+}
+else if (attemptsLeft ==0){
+            cout<< "   _____"<<endl;
+            cout<< "  |     |"<<endl;
+            cout<< "  |     O"<<endl;
+            cout<< "  |    /|"<<endl;
+            cout<< "  |    / \\ "<<endl;
+            cout<< "  |      "<<endl;
+            cout<< "  |      "<<endl; 
+}*/
+//yes i am just gon go and cry now
+//btw i hope this comes in handy cuz my 2 brai cells didnt think to copy and paste until trial 3
